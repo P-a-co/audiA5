24a2d6ed43d52ca6df7887c521e7247becb2f36a
FH_User <ic22b002@technikum-wien.at>
2022-11-09 19:15:43 +0100

    Initial commit

---
+++ src/main/java/com/example/audia5/AudiA5Application.java
@@ -0,0 +1,13 @@
+package com.example.audia5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AudiA5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AudiA5Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/audia5/AudiA5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.audia5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AudiA5ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
